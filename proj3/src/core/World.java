0f14c7d8634c8309ec6c711d34fa0ef3330da24b
Venu Poruri <vmporuri@gmail.com>
2023-11-05 14:45:48 -0800

    Add proj3 files

---
+++ proj3/src/core/World.java
@@ -0,0 +1,12 @@
+package core;
+
+import java.util.List;
+
+public class World {
+
+    // build your own world!
+    private final int maxLength = 50;
+    private final int maxHeight = 30;
+    private List<Room> rooms;
+
+}
