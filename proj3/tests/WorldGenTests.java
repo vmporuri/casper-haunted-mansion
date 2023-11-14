3a8b09548604b4b3b773051ab37f9305222ae171
Venu Poruri <vmporuri@gmail.com>
2023-11-13 22:19:26 -0800

    Add interactivity and persistence tests

--- proj3/tests/WorldGenTests.java
+++ proj3/tests/WorldGenTests.java
@@ -26,18 +26,66 @@ public class WorldGenTests {
     public void testSeed() {
         TETile[][] tiles1 = AutograderBuddy.getWorldFromInput("n1234567890123456789s");
         TETile[][] tiles2 = AutograderBuddy.getWorldFromInput("n1234567890123456789s");
-        assertThat(tiles1).isEqualTo(tiles2);
+        testEqualityOfTiles(tiles1, tiles2);
     }
 
     /** Basic interactivity test. */
     @Test
     public void basicInteractivityTest() {
-        // TODO: write a test that uses an input like "n123swasdwasd"
+        TETile[][] originalTiles = AutograderBuddy.getWorldFromInput("n123s");
+        TETile[][] onlyW = AutograderBuddy.getWorldFromInput("n123swww");
+        TETile[][] onlyS = AutograderBuddy.getWorldFromInput("n123ssss");
+        TETile[][] onlyA = AutograderBuddy.getWorldFromInput("n123saaa");
+        TETile[][] onlyD = AutograderBuddy.getWorldFromInput("n123sddd");
+        TETile[][] allMovements = AutograderBuddy.getWorldFromInput("n123swwasdd");
+
+        assertThat(originalTiles[63][12].character()).isEqualTo('@');
+        assertThat(onlyW[63][15].character()).isEqualTo('@');
+        assertThat(onlyW[63][12].character()).isEqualTo('·');
+        assertThat(onlyS[63][9].character()).isEqualTo('@');
+        assertThat(onlyS[63][12].character()).isEqualTo('·');
+        assertThat(onlyA[60][12].character()).isEqualTo('@');
+        assertThat(onlyA[63][12].character()).isEqualTo('·');
+        assertThat(onlyD[66][12].character()).isEqualTo('@');
+        assertThat(onlyD[63][12].character()).isEqualTo('·');
+        assertThat(allMovements[64][13].character()).isEqualTo('@');
+        assertThat(allMovements[63][12].character()).isEqualTo('·');
+    }
+
+    /** Tests that the avatar only moves when the WASD keys are pressed. */
+    @Test
+    public void wallsBlockPlayerTest() {
+        TETile[][] touchingWall = AutograderBuddy.getWorldFromInput("n123sssss");
+        TETile[][] tryingToMoveThroughWall = AutograderBuddy.getWorldFromInput("n123sssss");
+        testEqualityOfTiles(touchingWall, tryingToMoveThroughWall);
+    }
+
+    /** Tests that the avatar only moves when the WASD keys are pressed. */
+    @Test
+    public void validInputsTest() {
+        TETile[][] originalTiles = AutograderBuddy.getWorldFromInput("n123s");
+        TETile[][] tilesAfterNonWASDInput = AutograderBuddy.getWorldFromInput("n123sqertyuiop");
+        testEqualityOfTiles(originalTiles, tilesAfterNonWASDInput);
     }
 
     /** Basic persistence test. */
     @Test
     public void basicSaveTest() {
-        // TODO: write a test that calls getWorldFromInput twice, with "n123swasd:q" and with "lwasd"
+        TETile[][] tiles1 = AutograderBuddy.getWorldFromInput("n123swasd:q");
+        TETile[][] tiles2 = AutograderBuddy.getWorldFromInput("lwasd");
+        TETile[][] tiles3 = AutograderBuddy.getWorldFromInput("n123swwwwwwwwwww:q");
+        TETile[][] tiles4 = AutograderBuddy.getWorldFromInput("l:q");
+
+        testEqualityOfTiles(tiles1, tiles2);
+        testEqualityOfTiles(tiles3, tiles4);
+    }
+
+    /** Tests if two 2D matrices of tiles are equal. */
+    private void testEqualityOfTiles(TETile[][] tiles1, TETile[][] tiles2) {
+        for (int i = 0; i < tiles1.length; i++) {
+            for (int j = 0; j < tiles1[0].length; j++) {
+                assertThat(tiles1[i][j].character()).isEqualTo(tiles2[i][j].character());
+            }
+        }
     }
 }
